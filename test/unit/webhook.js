dc98fff552fdcfa0ec96df41733781206d52fdf2
Burns, Cody <cody.burns@accenture.com>
2019-04-27 10:36:03 -0500

    add initial webhook tests

---
+++ test/unit/webhook.js
@@ -0,0 +1,137 @@
+const assert = require('assert');
+const { expect } = require('chai');
+
+//* unit under test *//
+
+const app = require('../../index.js');
+
+/* Description:
+ *  tests the webhooks for basic response
+ */
+
+//* data *//
+
+var getBlockNumber = {"queryResult": {"intent": {"name": "","displayName": "etc_getBlockNumber"},"intentDetectionConfidence": 0.97,"languageCode": "en"}};
+var getBalance = {"queryResult": {"intent": {"name": "","displayName": "etc_getBalance"},"intentDetectionConfidence": 0.97,"languageCode": "en"}};
+var getTransaction= {"queryResult": {"intent": {"name": "","displayName": "etc_getTransaction"},"intentDetectionConfidence": 0.97,"languageCode": "en"}};
+var sendSignedTransaction = {"queryResult": {"intent": {"name": "","displayName": "etc_sendSignedTransaction"},"intentDetectionConfidence": 0.97,"languageCode": "en"}};
+var getGasPrice = {"queryResult": {"intent": {"name": "","displayName": "etc_getGasPrice"},"intentDetectionConfidence": 0.97,"languageCode": "en"}};
+var getBlock = {"queryResult": {"intent": {"name": "","displayName": "etc_getBlock"},"intentDetectionConfidence": 0.97,"languageCode": "en"}};
+var version = {"queryResult": {"intent": {"name": "","displayName": "etc_version"},"intentDetectionConfidence": 0.97,"languageCode": "en"}};
+
+module.exports =
+
+describe('Webhook Basic Tests', () => {
+    const app = 'http://localhost:'+ process.env.PORT +'/webhook';
+    
+    describe('Test getblocknumber', () => {
+        it('returns status 200 with a payload', (done) => {
+        request(app)
+        .post('/webhook/')
+        .send(getBlockNumber)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) done(err);
+          res.body.should.have.property('payload');
+          //res.body.participant.should.have.property('nuid', '98ASDF988SDF89SDF89989SDF9898');
+           });
+        done();
+        });
+    });
+    describe('Test getbalance', () => {
+        it('returns status 200 with a payload', (done) => {
+        request(app)
+        .post('/webhook/')
+        .send(getBalance)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) done(err);
+          res.body.should.have.property('payload');
+          //res.body.participant.should.have.property('nuid', '98ASDF988SDF89SDF89989SDF9898');
+           });
+        done();
+        });
+    });
+    
+    describe('Test getTransaction', () => {
+        it('returns status 200 with a payload', (done) => {
+        request(app)
+        .post('/webhook/')
+        .send(getTransaction)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) done(err);
+          res.body.should.have.property('payload');
+          //res.body.participant.should.have.property('nuid', '98ASDF988SDF89SDF89989SDF9898');
+           });
+        done();
+        });
+    });
+    
+    describe('Test sendSigned transaction', () => {
+        it('returns status 200 with a payload', (done) => {
+        request(app)
+        .post('/webhook/')
+        .send(sendSignedTransaction)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) done(err);
+          res.body.should.have.property('payload');
+          //res.body.participant.should.have.property('nuid', '98ASDF988SDF89SDF89989SDF9898');
+           });
+        done();
+        });
+    });
+    
+    describe('Test getgasprice', () => {
+        it('returns status 200 with a payload', (done) => {
+        request(app)
+        .post('/webhook/')
+        .send(getGasPrice)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) done(err);
+          res.body.should.have.property('payload');
+          //res.body.participant.should.have.property('nuid', '98ASDF988SDF89SDF89989SDF9898');
+           });
+        done();
+        });
+    });
+    
+    describe('Test getblock', () => {
+        it('returns status 200 with a payload', (done) => {
+        request(app)
+        .post('/webhook/')
+        .send(getBlock)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) done(err);
+          res.body.should.have.property('payload');
+          //res.body.participant.should.have.property('nuid', '98ASDF988SDF89SDF89989SDF9898');
+           });
+        done();
+        });
+    });
+    
+    describe('Test version', () => {
+        it('returns status 200 with a payload', (done) => {
+        request(app)
+        .post('/webhook/')
+        .send(version)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) done(err);
+          res.body.should.have.property('payload');
+          //res.body.participant.should.have.property('nuid', '98ASDF988SDF89SDF89989SDF9898');
+           });
+        done();
+        });
+    });
+});
