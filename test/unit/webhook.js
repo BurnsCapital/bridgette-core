3c25612607e7011d1c4e0f889ffe7ea76d2029a6
Burns, Cody <cody.burns@accenture.com>
2019-04-27 13:28:46 -0500

    expect

--- test/unit/webhook.js
+++ test/unit/webhook.js
@@ -34,10 +34,10 @@ describe('Webhook Basic Tests', () => {
         .post('/webhook')
         .send(getBlockNumber)
         .end(function(err, res) {
-            console.log("test res: "+ JSON.stringify(res.req));
+            console.log("test res: "+ JSON.stringify(res));
             if (err) done(err);
-            res.req.should.have.property('payload');
-            //res.body.participant.should.have.property('nuid', '98ASDF988SDF89SDF89989SDF9898');
+            expect(res).to.have.status(200);
+            //res.body.participant.shouldave.property('nuid', '98ASDF988SDF89SDF89989SDF9898');
              });
         done();
         });
